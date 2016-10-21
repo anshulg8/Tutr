8c8b87c48776fb19a089ad5a7b3896956fa9ebce
Anshul Garg <anshulgarg.garg@gmail.com>
2016-10-22 01:16:56 +0530

    Removes welcome controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   resources :posts
   root 'posts#index'
 
-  get 'welcome/about'
-  get 'about' => 'welcome#about'
-  get 'hello-world' => 'welcome#hello_world'
+  # get 'welcome/about'
+  # get 'about' => 'welcome#about'
+  # get 'hello-world' => 'welcome#hello_world'
 
   %w( 404 422 500 ).each do |code|
     get code, :to => "errors#show", :code => code
