2f8fc5598e668624c4d71593092340985dd05fe6
Anshul Garg <anshulgarg.garg@gmail.com>
2016-10-21 23:39:10 +0530

    Adds Posts CRUD, about page and model

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,9 @@
 Rails.application.routes.draw do
+  resources :posts
+  root 'posts#index'
 
-  get 'welcome/index'
   get 'welcome/about'
-  get 'welcome/hello-world'
-
-  root 'welcome#index'
-
   get 'about' => 'welcome#about'
-  get 'contact' => 'contacts#new'
   get 'hello-world' => 'welcome#hello_world'
 
   %w( 404 422 500 ).each do |code|
