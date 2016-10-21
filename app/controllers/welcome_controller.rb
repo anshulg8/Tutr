7f6bf724339838e56c32d3c935d3846995426cb2
Anshul Garg <anshulgarg.garg@gmail.com>
2016-10-21 22:54:18 +0530

    Adds favicon, helloworld page and page title helper method

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,7 @@
 class WelcomeController < ApplicationController
   def index
   end
+
+  def hello_world
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,13 @@ Rails.application.routes.draw do
 
   get 'welcome/index'
   get 'welcome/about'
-  get 'welcome/subscribe'
+  get 'welcome/hello-world'
 
   root 'welcome#index'
 
   get 'about' => 'welcome#about'
   get 'contact' => 'contacts#new'
-  get 'subscribe' => 'welcome#subscribe'
+  get 'hello-world' => 'welcome#hello_world'
 
   %w( 404 422 500 ).each do |code|
     get code, :to => "errors#show", :code => code
