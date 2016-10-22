0fb06a1f83a4723db86b02bd5c2db7d6c82a6b25
Anshul Garg <anshulgarg.garg@gmail.com>
2016-10-22 14:29:45 +0530

    Adds production gem

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,6 @@ Rails.application.routes.draw do
   resources :posts
   root 'posts#index'
 
-  # get 'welcome/about'
-  # get 'about' => 'welcome#about'
-  # get 'hello-world' => 'welcome#hello_world'
-
   %w( 404 422 500 ).each do |code|
     get code, :to => "errors#show", :code => code
   end
