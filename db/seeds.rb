6b3e77eaa050e0792940fd6250c4dc7a0d47da04
Anshul Garg <anshulgarg.garg@gmail.com>
2016-10-23 21:01:53 +0530

    Minor changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,18 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Post.create(title: 'test_post', content: 'Hi, this is a test post')
+Post.create(title: 'test_post', content: 'Hello World: Our first program will print the classic “hello world” message. Here’s the full source code.
+
+```python
+print ("Hello, World!")
+```
+Wait! What? Is that it? You bet it is. Congrats, You just wrote your first python program.
+
+If you are running older version of Python (Python 2.x), use of parenthesis as in print function is optional.
+
+To run the program, put the code in hello-world.py and type python hello-world.py in terminal. This produces the following result:
+
+**Screenshot**
+
+Now that we can run and build basic Python programs, let’s learn more about the language.
+Next example: Values.')
