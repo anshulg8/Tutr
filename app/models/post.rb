151aea595345a49c56da8ea96a336b8ead30e582
Anshul Garg <anshulgarg.garg@gmail.com>
2016-10-23 20:28:49 +0530

    Adds previous, next buttons

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,15 @@
 class Post < ActiveRecord::Base
+	
+	validates_uniqueness_of :title, :case_sensitive => false
+	
 	extend FriendlyId
   friendly_id :title, use: :slugged
+
+	def previous
+	  Post.where(["id < ?", id]).last
+	end
+
+	def next
+	  Post.where(["id > ?", id]).first
+	end
 end
